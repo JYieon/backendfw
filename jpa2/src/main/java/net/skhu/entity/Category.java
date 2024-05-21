4d4db6cfda5c50795e7e12aa4f66769ebc947ada
choijy <dkcl.8642@gmail.com>
2024-05-21 23:23:06 +0900

    JPA collection-valued association

--- jpa2/src/main/java/net/skhu/entity/Category.java
+++ jpa2/src/main/java/net/skhu/entity/Category.java
@@ -1,10 +1,14 @@
 package net.skhu.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+
 
 @Data
 @Entity
@@ -14,6 +18,9 @@ public class Category {
     int id;
 
     String name;
+
+    @OneToMany(mappedBy="category")
+    List<Book> books;
 
 }
 
