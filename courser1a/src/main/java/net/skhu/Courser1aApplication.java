047ed1cd978accc4678bee9822294dff7b182b34
choijy <dkcl.8642@gmail.com>
2024-05-22 13:06:59 +0900

    JPA CURD

---
+++ courser1a/src/main/java/net/skhu/Courser1aApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Courser1aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Courser1aApplication.class, args);
+	}
+
+}
